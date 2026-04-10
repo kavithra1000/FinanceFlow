1fd25f88c03f16c8b6de13546a41e7592382c407
Monal <monalwickramasinghe@gmail.com>
2026-04-10 16:24:15 +0530

    feat: add document upload page with initial header component

--- src/app/(document)/doc/page.tsx
+++ src/app/(document)/doc/page.tsx
@@ -2,7 +2,6 @@
 import { useState } from 'react'
 import { Inter } from 'next/font/google'
 import Header from '@/components/doc/Header'
-import FileUpload from '@/components/doc/FileUpload'
 
 
 const inter = Inter({
@@ -18,7 +17,6 @@ const Page = () => {
         <main className={`${inter.className} min-h-screen flex flex-col scroll-smooth`}>
             <section className='flex-1'>
                 <Header />
-                <FileUpload setSelectedFiles={setSelectedFiles} selectedFiles={selectedFiles} />
             </section>
 
         </main>
